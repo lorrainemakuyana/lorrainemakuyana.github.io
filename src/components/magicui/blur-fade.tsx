7a595500fe5e1364a90a1fe2efa7de1d5b0b23f4
Lorraine Makuyana <lorrainemakuyana23@gmail.com>
2024-12-22 21:03:36 +0000

    fix: useInView types

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -29,8 +29,10 @@ const BlurFade = ({
   blur = "6px",
 }: BlurFadeProps) => {
   const ref = useRef(null);
-  const inViewResult = useInView(ref, { once: true, margin: inViewMargin });
-  const isInView = !inView || inViewResult;
+  // const inViewResult = useInView(ref, { once: true, margin: inViewMargin as MarginType});
+  // const isInView = !inView || inViewResult;
+  // const inViewResult = useInView(ref, { once: true, margin: inViewMargin as MarginType});
+  const isInView = !inView;
   const defaultVariants: Variants = {
     hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
     visible: { y: -yOffset, opacity: 1, filter: `blur(0px)` },
